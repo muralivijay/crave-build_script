b0f8aaac203cdc45ae34a8a6024ede10decf7218
muralivijay <muralidharan9845@gmail.com>
2025-12-23 15:41:15 +0530

    Update script for spes

--- script.sh
+++ script.sh
@@ -1,17 +1,4 @@
 rm -rf .repo/local_manifests/
-rm -rf external/chromium-webview
-rm -rf out/soong out/host/linux-x86
-rm -rf hardware/qcom-caf/msm8998
-rm -rf hardware/qcom-caf/sdm660
-rm -rf device/asus/sdm660-common
-rm -rf vendor/asus
-
-# Symlink libncurses 6 >> 5
-sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5
-sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6   /usr/lib/x86_64-linux-gnu/libtinfo.so.5
-echo "============="
-echo "lib6 >> lib5  "
-echo "============="
 
 #repo init
 repo init -u https://github.com/crdroid-13-fork/android.git -b 13.0 --git-lfs
@@ -20,7 +7,7 @@ echo "Repo init success"
 echo "=================="
 
 #local_manifest
-git clone https://github.com/Kyura-Ground/local_manifests.git -b Rising13 .repo/local_manifests
+git clone https://github.com/muralivijay/crave-local_manifest/local_manifests.git -b 13.0 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -32,7 +19,7 @@ echo "Sync success"
 echo "============="
 
 # Export
-export BUILD_USERNAME=kyura
+export BUILD_USERNAME=muralivijay
 export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
 
@@ -41,4 +28,4 @@ echo "======= Export Done ======"
 echo "====== Envsetup Done ======="
 
 #build
-lunch lineage_X00TD-user && make installclean && m bacon
+lunch lineage_spes-user && make installclean && m bacon
